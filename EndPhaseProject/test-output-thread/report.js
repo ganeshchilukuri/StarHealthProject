f5a2e2778fce794fbde7e40b7703c2ce9d3026b6
ganeshchilukuri <148946571+ganeshchilukuri@users.noreply.github.com>
2023-12-01 19:52:15 +0530

    StarHealth

---
+++ EndPhaseProject/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"257cf75a-7aa9-4fe8-8ad2-918b0f7acd05","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1701416556965,"group":1,"content":"","tags":"","end":1701416596332,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
